2e679e698dd5bf432428de3668e326d810f4985d
Dev pathak <devpathak9685@gmail.com>
2026-05-21 23:51:52 +0530

    Added config-server(Not configured)

--- user-service/src/main/java/com/dev/sphere/userService/service/ProfileServiceImpl.java
+++ user-service/src/main/java/com/dev/sphere/userService/service/ProfileServiceImpl.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
